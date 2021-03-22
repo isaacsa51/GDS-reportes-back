90847e210524cbe1a87295d88113aba41814f38e
Isaac Serrano <serranoie99@gmail.com>
2021-03-22 13:22:49 -0700

    CRUD usuarios finalizada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,20 @@
 const { Router } = require("express");
 const router = Router();
 
-const {
-	getDBInfo,
-	getUsers,
-	createUser,
-} = require("../controllers/index.controller");
+const info = require("../controllers/index.controller");
+const users = require("../controllers/users.controller");
 
-router.get("/", getDBInfo);
-router.get("/users", getUsers);
-router.post("/users", createUser);
+router.get("/", info.infoAPI);
+
+// GET all users
+router.get("/users", users.getAll, users.getByID);
+// GET user by ID
+router.get("/users/:id", users.getByID);
+// CREATE a new user
+router.post("/users", users.create);
+// UPDATE a user by ID
+router.put("/users/:id", users.update);
+// DELETE a user by ID
+router.delete("/users/:id", users.delete);
 
 module.exports = router;
