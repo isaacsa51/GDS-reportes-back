bf40ae0d664c7d4c2f680dd1f6f9f528d46b9e8b
Isaac Serrano <serranoie99@gmail.com>
2021-03-22 15:26:40 -0700

    CRUD empresas finalizada

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,3 +1,24 @@
 exports.infoAPI = (req, res) => {
-	res.send("Rutas creadas: /users");
+	res.send(`
+    <div>
+        <h2>Rutas creadas dentro del API</h2>
+
+        <ul>
+            <li>/users</li>
+            <li>/users/[id_usuario]</li>
+                <ul>
+                    <li>GET para obtener un usuario por ID</li>
+                    <li>PUT para actualizar un usuario por ID</li>
+                    <li>DELETE para eliminar un usuario</li>
+                </ul>
+            <li>/companies</li>
+            <li>/companies/[id_empresa]</li>
+                <ul>
+                    <li>GET para obtener una empresa por ID</li>
+                    <li>PUT para actualizar la información por ID</li>
+                    <li>PUT para actualizar password</li>
+                    <li>DELETE para eliminar una empresa</li>
+                </ul>
+        </ul>
+    </div>`);
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,14 @@ const router = Router();
 
 const info = require("../controllers/index.controller");
 const users = require("../controllers/users.controller");
+const companies = require("../controllers/companies.controller");
 
+// ** BASIC INFO FROM THE API **
 router.get("/", info.infoAPI);
 
+// ** USERS **
 // GET all users
-router.get("/users", users.getAll, users.getByID);
+router.get("/users", users.getAll);
 // GET user by ID
 router.get("/users/:id", users.getByID);
 // CREATE a new user
@@ -16,5 +19,19 @@ router.post("/users", users.create);
 router.put("/users/:id", users.update);
 // DELETE a user by ID
 router.delete("/users/:id", users.delete);
+
+// ** COMPANIES **
+// Get all companies
+router.get("/companies", companies.getAll);
+// GET company by ID
+router.get("/companies/:id", companies.getByID);
+// CREATE a new company
+router.post("/companies", companies.create);
+// UPDATE a company by ID
+router.put("/companies/:id", companies.updateInfo);
+// UPDATE password company
+router.put("/companies/:id", companies.changePassword);
+// DELETE a user by ID
+router.delete("/companies/:id", companies.delete);
 
 module.exports = router;
